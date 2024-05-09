cc8d9caacfdd1c5270e537837cbe9bb7536601c7
powermedia0816 <jessicayang1004@gmail.com>
2024-05-09 16:38:33 +0800

    calculate words length avg

--- read.py
+++ read.py
@@ -6,7 +6,11 @@ with open("reviews.txt","r")as f:
 		count += 1 #count = count +1 # 
 		if count % 1000 == 0:
 			print(len(data))
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print("平均留言是",sum_len/len(data))
 
-print(data[0])
-print("-------------------------------------------------------------")
-print(data[1])
+
+
+# print("檔案讀取完了,總共有", len(data),"筆資料")
